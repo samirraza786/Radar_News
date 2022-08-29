934eb4c146eb1f1c9d7f061f27825f4378fe4da2
Vaibhav <vaibhavverma21122001@gmai.com>
2022-08-29 16:19:33 +0530

    structure of App

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <>
+        <p>this is a new item.</p>
+      </>
+    )
+  }
+}
+
+export default NewsItem;
