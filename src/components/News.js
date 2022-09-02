da1ec380e911ea7a5f29611ee4a08110b63a97cc
Vaibhav <vaibhavverma21122001@gmai.com>
2022-09-02 21:47:42 +0530

    Adding time, author and news source

--- src/components/News.js
+++ src/components/News.js
@@ -91,7 +91,7 @@ export class News extends Component {
                         {/* render news content when loading is false */}
                         {!this.state.loading && this.state.articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author = {element.author} publishedAt = {element.publishedAt} source = {element.source.name} />
                             </div>
                         })}
                     </div>
